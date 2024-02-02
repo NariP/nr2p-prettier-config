45c2f88173c1c87559af39d8543e815adca22b53
narip <narip@github.com>
2024-02-02 21:22:16 +0900

    feat: init setting

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# nr2p-prettier-config
+# @nr2p/prettier-config
+
+```agsl
+내가 자주 사용하는 prettier config
+```
---
+++ index.js
@@ -0,0 +1,12 @@
+/** @type {import('prettier').Options} */
+module.exports = {
+    singleQuote: true,
+    jsxSingleQuote: false,
+    trailingComma: 'es5',
+    printWidth: 100,
+    tabWidth: 2,
+    semi: true,
+    endOfLine: 'auto',
+    arrowParens: 'avoid',
+    bracketSpacing: true
+};
